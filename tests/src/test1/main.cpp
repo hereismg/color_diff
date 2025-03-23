ed0fb2554972224c64d40da03143caaba55340b4
hereismg <hereismg@163.com>
2025-03-23 23:02:13 +0800

    update: 搭建测试框架

--- tests/src/test1/main.cpp
+++ tests/src/test1/main.cpp
@@ -3,6 +3,19 @@
 using namespace mg;
 using namespace std;
 
+TEST(Test_data_path, _1){
+#ifndef TEST_DATA_PATH
+    cout << "未定义宏 TEST_DATA_PATH" << endl;
+    ASSERT_TRUE(false);
+#else
+    ASSERT_TRUE(true);
+#endif
+}
+std::string data_dir = TEST_DATA_PATH;
+
 TEST(TestSuit, _1){
-    cout << "Hello GTest!" << endl;
+    auto befor = read(data_dir + "/befor.txt");
+    auto after = read(data_dir +  "/after.txt");
+
+    Text txt(befor, after);
 }
